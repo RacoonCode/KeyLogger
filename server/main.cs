add2524ca3fa8ec420c1ad148503ec1d9c5ff273
QwertyAscii <QwertyAscii@github.com>
2021-06-06 18:25:59 +0300

    Add project files

---
+++ server/main.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class TcpListener;
+
+namespace tcpKeylogger{
+    
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Test");
+            Console.ReadKey();
+        }
+    }
+}
